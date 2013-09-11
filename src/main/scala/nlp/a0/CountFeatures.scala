0b4a27d0bd257ef19d71991d4b72d95daafb43d8
Sudheesh Katkam <sudheesh.katkam@utexas.edu>
2013-09-11 11:26:13 -0500

    started #6

--- src/main/scala/nlp/a0/CountFeatures.scala
+++ src/main/scala/nlp/a0/CountFeatures.scala
@@ -5,13 +5,19 @@ import io.Source
 object CountFeatures {
   
   def main(args: Array[String]) : Unit = {
-    val lines =  Source.fromFile(args(0)).getLines
-    lines.map(handle)
+    // "A=B, C=D, ... L"
+    // Map[String, Map [String, Map [String, Int]]]
+    //     feature      label        value   count
+    val lines =  Source.fromFile(args(0)).getLines.toVector
+    println (lines.map(handle).groupBy(x => x._1).mapValues(x => x))
   }
   
-  def handle(line: String): Vector[Tuple2[String, Vector[Tuple2[String,String]]]] = {
-    // [(L1, [(A,B), (C, D), ...]) ...]
-    
-    ???
+  def handle(line: String): Tuple2[String, Vector[Tuple2[String,String]]] = {
+    // A=B, C=D, .. L1 
+    // [ (L1, [(A, B), (C, D), ...]) ...]
+    val fields = line.split(",").toVector
+    val label = fields.last
+    val rest = fields.dropRight(1).map(x => x.split("=")).map(x => (x(0), x(1)))
+    (label, rest)
   }
 }
