a7a1a3eba8a8ee196d269455ae596014f58cf940
Sudheesh Katkam <sudheesh.katkam@utexas.edu>
2013-12-03 22:55:17 -0600

    done with part 4.

--- src/main/scala/nlp/a6/PcfgParser.scala
+++ src/main/scala/nlp/a6/PcfgParser.scala
@@ -2,15 +2,21 @@ package nlp.a6
 
 import nlpclass.Parser
 import nlpclass.Tree
+import nlp.a1.ConditionalProbabilityDistribution
+import nlp.a1.ProbabilityDistribution
 
-class PcfgParser extends Parser {
+class PcfgParser(prodProbDist: ConditionalProbabilityDistribution[String, Vector[Tree]],
+                 rootProbDist: ProbabilityDistribution[String]) extends Parser {
 
   def likelihood(t: Tree): Double = {
-    // your code here
-    ???
+    val tree = Cnf.convertTree(t)
+    val subTrees = TreeUtilities.allSubTrees(tree)
+    val prodProb = subTrees.map { st => math.log(prodProbDist(st.children, st.label)) }.reduce(_ + _)
+    return math.log(rootProbDist(t.label)) + prodProb
   }
 
   def parse(tokens: Vector[String]): Option[Tree] = ???
+
   def generate(): Tree = ???
 
 }
--- src/main/scala/nlp/a6/UnsmoothedPcfgParserTrainer.scala
+++ src/main/scala/nlp/a6/UnsmoothedPcfgParserTrainer.scala
@@ -2,12 +2,26 @@ package nlp.a6
 
 import nlpclass.ParserTrainer
 import nlpclass.Tree
+import nlp.a1.ConditionalProbabilityDistribution
+import nlp.a1.ProbabilityDistribution
+import nlpclass.TreeNode
 
-class UnsmoothedPcfgParserTrainer() extends ParserTrainer {
+class UnsmoothedPcfgParserTrainer extends ParserTrainer {
 
   def train(trees: Vector[Tree]): PcfgParser = {
-    // your code here
-    ???
+
+    val rootData = trees.map { t => t.label }
+    val rootProbDist = new ProbabilityDistribution(rootData)
+
+    val subTrees = trees.map { t =>
+      val tree = Cnf.convertTree(t)
+      TreeUtilities.allSubTrees(tree)
+    }.flatten
+
+    val prodData = subTrees.map { tree => (tree.label, tree.children) }
+    val prodProbDist = new ConditionalProbabilityDistribution(prodData)
+
+    return new PcfgParser(prodProbDist, rootProbDist)
   }
-  
+
 }
